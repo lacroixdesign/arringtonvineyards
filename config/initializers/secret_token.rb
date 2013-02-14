998a16f2f7db93d72092973d13b9def9772d2693
Michael LaCroix <michael@lacroixdesign.net>
2013-02-14 16:12:39 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArringtonVineyards::Application.config.secret_token = '72942fa43bd56ac109225c61a1df0bacd963be39d9898d2bcfcf772c73e208e51e83741517b139b27558663b3c6c32ee39e0328165c7721e03c939337b68a0d0'
