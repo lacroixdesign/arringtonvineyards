602c813224ae4aefc0a94c0b603408ef90582605
Michael LaCroix <michael@lacroixdesign.net>
2013-02-17 18:18:53 -0600

    consolidate nav & add & mobile layout; clean up some prototyping

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 ArringtonVineyards::Application.routes.draw do
-  get "/v2" => "pages#v2"
-  get "/v3" => "pages#v3"
-  get "/v4" => "pages#v4"
   root :to => "pages#index"
 
   mount RailPass::Engine, :at => "styleguide"
