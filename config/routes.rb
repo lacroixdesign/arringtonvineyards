b5547a9da75fd3f57d618b929814132b81a7b324
Michael LaCroix <michael@lacroixdesign.net>
2013-02-21 16:30:48 -0600

    add Page#show prototype

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 ArringtonVineyards::Application.routes.draw do
-  get "background" => "pages#background"
-  get "horizontal" => "pages#horizontal"
-  get "grid" => "pages#grid"
+  get ":action", controller: "pages"
   root :to => "pages#index"
 
   mount RailPass::Engine, :at => "styleguide"
