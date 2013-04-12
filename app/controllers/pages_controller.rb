0ba4c547120f7bf8d1ca1a42f3a6daed3a3e988d
Michael LaCroix <michael@lacroixdesign.net>
2013-04-12 11:22:38 -0500

    remove unused gallery prototypes, switch home page to final choice

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,2 @@
 class PagesController < ApplicationController
-  def index
-    @alt_footer = true
-    @images1 = (1..9).map {|n| "http://arrington-vineyards-dev.s3.amazonaws.com/prototype/gallery/bg-#{n}.jpg" }
-    @images2 = (10..20).map {|n| "http://arrington-vineyards-dev.s3.amazonaws.com/prototype/gallery/bg-#{n}.jpg" }
-    @gallery = [
-      {images: @images1},
-      {images: @images2}
-    ]
-  end
 end
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,2 @@
 class PrototypesController < ApplicationController
-  def background
-    @alt_footer = true
-  end
 end
